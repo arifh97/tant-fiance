98d9b65f87163ef136e38c42565eca1b793b75ef
arifh97 <arifline16@gmail.com>
2024-05-16 03:27:52 +0600

    component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from "react"
 import Header from './components/Header'
+import Banner from './components/Bannner'
+import Brands from './components/Brands'
+import About from './components/About'
+import HowToWork from './components/HowToWork'
+import RoadMap from './components/RoadMap'
+import Specification from './components/Specification'
+import Faq from './components/Faq'
+import WhyUs from './components/WhyUs'
+import Cta from './components/Cta'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <>
       <Header />
+      <Banner />
+      <Brands />
+      <About />
+      <HowToWork />
+      <RoadMap />
+      <Specification />
+      <Faq />
+      <WhyUs />
+      <Cta />
+      <Footer />
     </>
   )
 }
