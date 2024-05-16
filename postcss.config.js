c36e274620bb756bd99f205edbf81e00c691310a
arifh97 <arifline16@gmail.com>
2024-05-17 03:14:53 +0600

    features

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,10 @@ export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        'postcss-url': {
-            url: 'inline', // Inline images as data URLs
-            maxSize: 10, // Maximum file size to inline (in kilobytes)
-            fallback: 'copy', // Fallback for larger files
-        },
+        // 'postcss-url': {
+        //     url: 'inline', // Inline images as data URLs
+        //     maxSize: 10, // Maximum file size to inline (in kilobytes)
+        //     fallback: 'copy', // Fallback for larger files
+        // },
     },
 }
