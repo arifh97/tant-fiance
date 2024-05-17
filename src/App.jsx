66f57c104e1a146a265635b90982753ebbacab45
arifh97 <arifline16@gmail.com>
2024-05-17 20:04:09 +0600

    why us

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
       {/* <HowToWork /> */}
       <RoadMap />
       <Specification />
-      {/* <WhyUs /> */}
+      <WhyUs />
       <Faq />
       {/* <Cta /> */}
       {/* <Footer /> */}
