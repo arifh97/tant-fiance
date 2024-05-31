a3492a2e73cd49d1529b00d62399137877b33f85
arifh97 <arifline16@gmail.com>
2024-05-31 23:50:50 +0600

    new pages

--- src/components/Dashboard/MainContent.jsx
+++ src/components/Dashboard/MainContent.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function MainContent() {
   return (
-    <div className="p-5 md:p-7 lg:p-9">
+    <div className="dashboard-main p-5 md:p-7 lg:p-9 overflow-auto">
       <Outlet />
     </div>
   )
