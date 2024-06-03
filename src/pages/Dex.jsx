027f91e8cfd47397fc075ceabdc271f7dee07ecb
Hassan Ali <hassan4np@gmail.com>
2024-06-03 18:03:39 +0600

    complate dex section

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -3,8 +3,82 @@ import dexicon2 from "../assets/img/dexicon.png";
 import dexicon3 from "../assets/img/dexicon2.png";
 import dexicon4 from "../assets/img/dexicon3.png";
 import bitcoin from "../assets/img/Bitcoin.svg";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
+import icon1 from "../assets/img/ticon1.svg"
+import icon2 from "../assets/img/ticon2.svg"
+import icon3 from "../assets/img/ticon3.svg"
+import icon4 from "../assets/img/ticon4.svg"
+import icon5 from "../assets/img/ticon5.svg"
+import icon6 from "../assets/img/ticon6.svg"
+import icon7 from "../assets/img/ticon7.svg"
+import icon8 from "../assets/img/ticon8.svg"
+
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
 export default function Dex() {
+  const tabledata = [
+    {
+      title: "Bitcoin",
+      icon: `${icon1}`,
+      volume: "$10,000,00",
+      Profit: "$50,0000",
+      Signup: "100",
+    },
+    {
+      title: "Ethereum",
+      icon: `${icon2}`,
+      volume: "$5,000,00",
+      Profit: "$50,0000",
+      Signup: "80",
+    },
+    {
+      title: "USDT",
+      icon: `${icon3}`,
+      volume: "$2,000,00",
+      Profit: "$50,0000",
+      Signup: "70",
+    },
+    {
+      title: "Ripple",
+      icon: `${icon4}`,
+      volume: "$10,000,00",
+      Profit: "$1,000,00",
+      Signup: "65",
+    },
+    {
+      title: "USDC",
+      icon: `${icon5}`,
+      volume: "$10,000,00",
+      Profit: "$100,000",
+      Signup: "60",
+    },
+    {
+      title: "BNB",
+      icon: `${icon6}`,
+      volume: "$10,000,00",
+      Profit: "$80,000",
+      Signup: "50",
+    },
+    {
+      title: "Dodge",
+      icon: `${icon7}`,
+      volume: "$10,000,00",
+      Profit: "$50,000",
+      Signup: "40",
+    },
+    {
+      title: "Litecoin",
+      icon: `${icon8}`,
+      volume: "$10,000,00",
+      Profit: "$30,000",
+      Signup: "30",
+    },
+  ];
 
   return (
     <div className="dex">
@@ -28,10 +102,10 @@ export default function Dex() {
           </div>
           <div>
             <p className="text-lg leading-6 font-medium text-[#6B7280] mb-4 uppercase ">
-            Profit Today
+              Profit Today
             </p>
             <p className="text-[30px]  leading-[40px]  font-semibold text-[#D9DCE4]">
-            $2000
+              $2000
             </p>
           </div>
         </div>
@@ -41,10 +115,10 @@ export default function Dex() {
           </div>
           <div>
             <p className="text-lg leading-6 font-medium text-[#6B7280] mb-4 uppercase ">
-            Signups
+              Signups
             </p>
             <p className="text-[30px]  leading-[40px]  font-semibold text-[#D9DCE4]">
-            200
+              200
             </p>
           </div>
         </div>
@@ -54,50 +128,67 @@ export default function Dex() {
           </div>
           <div>
             <p className="text-lg leading-6 font-medium text-[#6B7280] mb-4 uppercase ">
-            User Registered
+              User Registered
             </p>
             <p className="text-[30px]  leading-[40px]  font-semibold text-[#D9DCE4]">
-            122
+              122
             </p>
           </div>
         </div>
       </div>
+
       <div>
-      <Table removeWrapper aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn className=" py-3 md:py-[18px] pl-6 text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">Coin</TableColumn>
-        <TableColumn className=" py-3 md:py-[18px] pl-6 text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">Volume (USD)</TableColumn>
-        <TableColumn className=" py-3 md:py-[18px] pl-6 text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">Profit (USD)</TableColumn>
-        <TableColumn className=" py-3 md:py-[18px] pl-6 text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">Signup</TableColumn>
-      </TableHeader>
-      <TableBody>
-        <TableRow key="1">
-          <TableCell className="flex items-center text-sm md:text-base leading-5 font-medium text-white py-2 md:py-4 px-5 md:px-6 bg-[#080B13]  mb-[5px] "> <img className="mr-[12px] h-8 w-8 md:h-10 md:w-10 " src={bitcoin} alt="" />Tony Reichert</TableCell>
-          <TableCell>CEO</TableCell>
-          <TableCell>Active</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="2">
-          <TableCell>Zoey Lang</TableCell>
-          <TableCell>Technical Lead</TableCell>
-          <TableCell>Paused</TableCell>
-          <TableCell>Paused</TableCell>
-        </TableRow>
-        <TableRow key="3">
-          <TableCell>Jane Fisher</TableCell>
-          <TableCell>Senior Developer</TableCell>
-          <TableCell>Active</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="4">
-          <TableCell>William Howard</TableCell>
-          <TableCell>Community Manager</TableCell>
-          <TableCell>Vacation</TableCell>
-          <TableCell>Vacation</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+        <Table removeWrapper aria-label="Example static collection table">
+          <TableHeader>
+            <TableColumn className=" py-3 md:py-[18px]  text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">
+              Coin
+            </TableColumn>
+            <TableColumn className=" py-3 md:py-[18px]  text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">
+              Volume (USD)
+            </TableColumn>
+            <TableColumn className=" py-3 md:py-[18px]  text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">
+              Profit (USD)
+            </TableColumn>
+            <TableColumn className=" py-3 md:py-[18px]  text-[10px] md:text-sm leading-[14px] font-medium text-[#6B7280]  bg-[#080B13]  mb-[5px] ">
+              Signup
+            </TableColumn>
+          </TableHeader>
+          <TableBody>
+          {
+            tabledata.map((item,idx)=>(<TableRow key={idx} className=" mb-5">
+            <TableCell className="bg-[#080B13] mb-5 ">
+              <div className="flex items-center -ml-6  md:pl-6 w-full text-sm md:text-base leading-5 font-medium text-white">
+                <img
+                  className=" mr-1 h-10 w-10 md:h-12 md:w-12 py-2"
+                  src={item.icon}
+                  alt=""
+                />
+                {item.title}
+              </div>
+            </TableCell>
+            <TableCell className="bg-[#080B13] mb-[5px]">
+              <p className="text-sm md:text-base -ml-5 mt-2 leading-5 font-medium text-white py- px-5 mb-4">
+                {item.volume}
+              </p>
+            </TableCell>
+            <TableCell className="bg-[#080B13] mb-[5px]">
+              <p className="text-sm md:text-base -ml-5 mt-3  leading-5 font-medium text-white py- px-5 mb-5">
+                {item.Profit}
+              </p>
+            </TableCell>
+            <TableCell className="bg-[#080B13] mb-[5px]">
+              <p className="text-sm md:text-base -ml-5  leading-5 font-medium text-white py- px-5">
+                {item.Signup}
+              </p>
+            </TableCell>
+          </TableRow>))
+          }
+
+       
+          </TableBody>
+        </Table>
       </div>
+
     </div>
   );
 }
