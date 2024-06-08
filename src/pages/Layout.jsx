b3ef9287cfc09e49a68c557ca0d5a34c5aa72e10
Hassan Ali <hassan4np@gmail.com>
2024-06-08 09:41:42 +0600

    update code

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Menu from '../components/Dashboard/Menu'
 import Header from '../components/Dashboard/Header'
 import MainContent from '../components/Dashboard/MainContent'
+import {useLocation } from 'react-router-dom'
 
 export default function Layout() {
   const [mobile,setMobile] = useState(false);
   const mobileMenu = () => {
       setMobile(!mobile)
   }
+  const pathname = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+   },[pathname]);
   return (
     <div className="dashboard flex flex-wrap min-h-screen">
         <div className={`dashboard-left ${mobile?'menu-show':''}`}>
